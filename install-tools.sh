a223d6493fffa2220a5a2f19bd5c7b89029ad77e
mrtrkmn <mr.turkmen@icloud.com>
2022-04-27 19:57:25 +0200

    add vscode goland go vagrant  docker

--- install-tools.sh
+++ install-tools.sh
@@ -26,7 +26,6 @@ else
     echo "* htop"
     echo "* tree"
     echo "* virtualbox"
-    echo "* vagrant"
 
     for i in "${!PROGRAMS[@]}"; do
        IS_EXISTS ${PROGRAMS[$i]}
@@ -54,6 +53,74 @@ else
         echo "Exiting without installing docker engine"
         exit 1
     fi
-      
-fi
 
+    echo "Would you like to install vagrant ? (y/n)"
+    if [  "$response" != "${response#[Yy]}" ]; then
+        if command -v vagrant >/dev/null 
+        then
+            echo "Vagrant already exists"
+        else
+            echo "Installing Vagrant..."
+            curl -fsSL https://raw.githubusercontent.com/hashicorp/vagrant/master/keys/vagrant.pub -o /home/$USER/.ssh/authorized_keys
+            curl -fsSL https://releases.hashicorp.com/vagrant/2.2.5/vagrant_2.2.5_x86_64.deb -o /tmp/vagrant.deb
+            dpkg -i /tmp/vagrant.deb
+            rm /tmp/vagrant.deb
+        fi
+    else
+        echo "Exiting without installing vagrant"
+        exit 1
+    fi
+
+    echo "Would you like to install vs code ? (y/n)"
+    if [  "$response" != "${response#[Yy]}" ]; then
+        if command -v code >/dev/null 
+        then
+            echo "VSCode already exists"
+        else
+            echo "Installing VSCode..."
+            curl -L https://go.microsoft.com/fwlink/?LinkID=760868 -o /tmp/vscode.deb
+            dpkg -i /tmp/vscode.deb
+            rm /tmp/vscode.deb
+        fi
+    else
+        echo "Exiting without installing vs code"
+        exit 1
+    fi
+
+    echo "Would you like to install go ? (y/n)"
+    if [  "$response" != "${response#[Yy]}" ]; then
+        if command -v go >/dev/null 
+        then
+            echo "Go already exists"
+        else
+            echo "Installing Go 1.18"
+            curl -fsSL https://golang.org/dl/go1.18.linux-amd64.tar.gz -o /tmp/go.tar.gz
+            tar -C /usr/local -xzf /tmp/go.tar.gz
+            rm /tmp/go.tar.gz
+            echo "Adding go to path..."
+            echo "export PATH=$PATH:/usr/local/go/bin" >> /home/$USER/.bashrc
+        fi
+    else
+        echo "Exiting without installing go"
+        exit 1
+    fi    
+
+    echo "Would you like to install goland ? (y/n)"
+    if [  "$response" != "${response#[Yy]}" ]; then
+        if command -v goland >/dev/null 
+        then
+            echo "Goland already exists"
+        else
+            echo "Installing Goland..."
+            curl -fsSL https://download.jetbrains.com/go/goland-2022.1.tar.gz -o /tmp/goland.tar.gz
+            tar -C /opt -xzf /tmp/goland.tar.gz
+            rm /tmp/goland.tar.gz
+            echo "Adding goland to path..."
+            echo "export PATH=$PATH:/opt/goland-2022.1/bin" >> /home/$USER/.bashrc
+        fi
+    else
+        echo "Exiting without installing goland"
+        exit 1
+    fi
+
+fi
