9670dbe641803e360577f60de47261f6dad9e692
mrtrkmn <mr.turkmen@icloud.com>
2022-04-27 19:47:45 +0200

    add docker installation

--- install-tools.sh
+++ install-tools.sh
@@ -4,7 +4,7 @@
 
 PROGRAMS=(git vim zsh curl wget tree htop virtualbox) 
 IS_EXISTS() {
-    if command -v $i >/dev/null 
+    if command -v $1 >/dev/null 
     then
         echo "$1 already exists"
     else
@@ -28,7 +28,32 @@ else
     echo "* virtualbox"
     echo "* vagrant"
 
-   for i in "${!PROGRAMS[@]}"; do
+    for i in "${!PROGRAMS[@]}"; do
        IS_EXISTS ${PROGRAMS[$i]}
-   done 
+    done 
+
+    echo "Would you like to install docker engine ? (y/n)"
+    read response
+    if [  "$response" != "${response#[Yy]}" ]; then
+        if command -v docker >/dev/null 
+        then
+            echo "Docker already exists"
+        else
+            echo "Installing Docker..."
+            curl -fsSL https://get.docker.com -o get-docker.sh
+            sh get-docker.sh
+            echo "Adding $USER to docker group..."
+            usermod -aG docker $USER
+            echo "Enabling docker service..."
+            systemctl enable docker
+            echo "Starting docker service..."
+            systemctl start docker
+            rm get-docker.sh
+        fi
+    else
+        echo "Exiting without installing docker engine"
+        exit 1
+    fi
+      
 fi
+
