7dadb3088504a22740927428c34006508b780fa0
mrtrkmn <mr.turkmen@icloud.com>
2022-04-28 00:46:47 +0200

    change to yellow; update vagrant version

--- install-tools.sh
+++ install-tools.sh
@@ -13,41 +13,41 @@ PROGRAMS=( "goland:INSTALL_GOLAND"
 BLUE="\033[0;34m"
 RED="\033[0;31m"
 GREEN="\033[0;32m"
+YELLOW="\033[1;33m"
 NC="\033[0m" 
 
 INSTALL_GOLAND() {
-   printf "${BLUE}Installing Go 1.18"
+   printf "${YELLOW}Installing Go 1.18\n"
    curl -fsSL https://golang.org/dl/go1.18.linux-amd64.tar.gz -o /tmp/go.tar.gz
    tar -C /usr/local -xzf /tmp/go.tar.gz
    rm /tmp/go.tar.gz
    printf "${RED}DO NOT FORGET TO ADD go to path...\n"
-   printf "${RED}hint: export PATH=$PATH:/usr/local/go/bin  >> /home/$USER/.bashrc${NC}" 
+   printf "${RED}hint: export PATH=$PATH:/usr/local/go/bin  >> /home/$USER/.bashrc${NC}\n" 
 }
 
 INSTALL_GO() {
-    printf "${BLUE}Installing Goland...${NC}\n"
+    printf "${YELLOW}Installing Goland...${NC}\n"
     curl -fsSL https://download.jetbrains.com/go/goland-2022.1.tar.gz -o /tmp/goland.tar.gz
     tar -C /opt -xzf /tmp/goland.tar.gz
     rm /tmp/goland.tar.gz
 }
 
 INSTALL_VAGRANT() {
-    printf "${BLUE}Installing Vagrant...${NC}\n"
-    curl -fsSL https://raw.githubusercontent.com/hashicorp/vagrant/master/keys/vagrant.pub -o /home/$USER/.ssh/authorized_keys
-    curl -fsSL https://releases.hashicorp.com/vagrant/2.2.5/vagrant_2.2.5_x86_64.deb -o /tmp/vagrant.deb
+    printf "${YELLOW}Installing Vagrant...${NC}\n"
+    curl -fsSL https://releases.hashicorp.com/vagrant/2.2.19/vagrant_2.2.19_x86_64.deb -o /tmp/vagrant.deb
     dpkg -i /tmp/vagrant.deb
     rm /tmp/vagrant.deb
 }
 
 INSTALL_DOCKER_ENGINE() {
-    printf "${BLUE}Installing Docker...${NC}\n"
+    printf "${YELLOW}Installing Docker...${NC}\n"
     curl -fsSL https://get.docker.com -o get-docker.sh
     sh get-docker.sh
-    printf "${BLUE}Adding $USER to docker group...\n"
+    printf "${YELLOW}Adding $USER to docker group...\n"
     usermod -aG docker $USER
-    printf "${BLUE} Enabling docker service...\n"
+    printf "${YELLOW} Enabling docker service...\n"
     systemctl enable docker
-    printf "${BLUE} Starting docker service...${NC}\n"
+    printf "${YELLOW} Starting docker service...${NC}\n"
     systemctl start docker
     rm get-docker.sh
 }
@@ -63,9 +63,9 @@ else
     printf "${BLUE}Updating apt-get...${NC}\n"
     apt update 
     printf "${GREEN}--------------------------------------------------------${NC}\n"
-    printf "${BLUE}This script will install the following packages: ${NC}\n"
-    printf "[ ${BLUE}${ESSENTIALS[*]}${NC} ]\n\n"
-    printf "${BLUE}Would like to continue ? [y/n] ${NC}\n"
+    printf "${YELLOW}This script will install the following packages: ${NC}\n"
+    printf "[ ${YELLOW}${ESSENTIALS[*]}${NC} ]\n\n"
+    printf "${YELLOW}Would like to continue ? [y/n] ${NC}\n"
     read -r response
     if [  "$response" != "${response#[Yy]}" ]; then
         for i in "${ESSENTIALS[@]}" ; do
@@ -92,7 +92,7 @@ else
         fi
     done
    
-   printf "${BLUE}--------------------------------------------------------${NC}\n\n"
+   printf "${YELLOW}--------------------------------------------------------${NC}\n\n"
 
    printf "${GREEN}Thanks for using the script ! ${NC}\n"
    printf "${GREEN}********************************************************${NC}\n"
