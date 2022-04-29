8c95d23d0927e2ca8b98098f36c6876f93f408d9
mrtrkmn <mr.turkmen@icloud.com>
2022-04-29 23:55:08 +0200

    remove commented lines

--- install-tools.sh
+++ install-tools.sh
@@ -26,78 +26,78 @@ NC="\033[0m"
 
 INSTALL_GO() {
    printf "${YELLOW}Installing Go 1.18\n"
-#    curl -fsSL https://golang.org/dl/go1.18.linux-amd64.tar.gz -o /tmp/go.tar.gz
-#    tar -C /usr/local -xzf /tmp/go.tar.gz
-#    rm /tmp/go.tar.gz
+   curl -fsSL https://golang.org/dl/go1.18.linux-amd64.tar.gz -o /tmp/go.tar.gz
+   tar -C /usr/local -xzf /tmp/go.tar.gz
+   rm /tmp/go.tar.gz
    printf "${RED}DO NOT FORGET TO ADD go to path...\n"
    printf "${RED}hint: export PATH=$PATH:/usr/local/go/bin  >> /home/$USER/.bashrc${NC}\n" 
 }
 
 INSTALL_PYCHARM() {
     printf "${YELLOW}Installing PyCharm\n"
-    # wget https://download.jetbrains.com/python/pycharm-community-2022.1.tar.gz
-    # tar -xzf pycharm-community-2022.1.tar.gz
-    # rm pycharm-community-2022.1.tar.gz
-    # mv pycharm-community-2022.1 /opt/pycharm
+    wget https://download.jetbrains.com/python/pycharm-community-2022.1.tar.gz
+    tar -xzf pycharm-community-2022.1.tar.gz
+    rm pycharm-community-2022.1.tar.gz
+    mv pycharm-community-2022.1 /opt/pycharm
 }
 
 INSTALL_RUBYMINE() {
     printf "${YELLOW}Installing RubyMine\n"
-    # wget https://download.jetbrains.com/ruby/RubyMine-2022.1.tar.gz
-    # tar -xzf RubyMine-2022.1.tar.gz
-    # rm RubyMine-2022.1.tar.gz
-    # mv RubyMine-2022.1 /opt/rubymine
+    wget https://download.jetbrains.com/ruby/RubyMine-2022.1.tar.gz
+    tar -xzf RubyMine-2022.1.tar.gz
+    rm RubyMine-2022.1.tar.gz
+    mv RubyMine-2022.1 /opt/rubymine
 }
 
 INSTALL_SUBLIME() {
     printf "${YELLOW}Installing Sublime Text\n"
-    # wget https://download.sublimetext.com/sublime-text_build-3126_amd64.deb
-    # sudo dpkg -i sublime-text_build-3126_amd64.deb
-    # rm sublime-text_build-3126_amd64.deb
+    wget https://download.sublimetext.com/sublime-text_build-3126_amd64.deb
+    sudo dpkg -i sublime-text_build-3126_amd64.deb
+    rm sublime-text_build-3126_amd64.deb
 }
 
 
 INSTALL_INTELLIJ() {
     printf "${YELLOW}Installing IntelliJ\n"
-    # wget https://download.jetbrains.com/idea/ideaIU-2022.1.tar.gz
-    # tar -xzf ideaIU-2022.1.tar.gz
-    # rm ideaIU-2022.1.tar.gz
-    # mv idea-IU-2022.1 /opt/intellij
+    wget https://download.jetbrains.com/idea/ideaIU-2022.1.tar.gz
+    tar -xzf ideaIU-2022.1.tar.gz
+    rm ideaIU-2022.1.tar.gz
+    mv idea-IU-2022.1 /opt/intellij
 }
 
 
 INSTALL_GOLAND() {
     printf "${YELLOW}Installing Goland...${NC}\n"
-    # curl -fsSL https://download.jetbrains.com/go/goland-2022.1.tar.gz -o /tmp/goland.tar.gz
-    # tar -C /opt -xzf /tmp/goland.tar.gz
-    # rm /tmp/goland.tar.gz
+    curl -fsSL https://download.jetbrains.com/go/goland-2022.1.tar.gz -o /tmp/goland.tar.gz
+    tar -C /opt -xzf /tmp/goland.tar.gz
+    rm /tmp/goland.tar.gz
 }
 
 INSTALL_VAGRANT() {
     printf "${YELLOW}Installing Vagrant...${NC}\n"
-    # curl -fsSL https://releases.hashicorp.com/vagrant/2.2.19/vagrant_2.2.19_x86_64.deb -o /tmp/vagrant.deb
-    # dpkg -i /tmp/vagrant.deb
-    # rm /tmp/vagrant.deb
+    curl -fsSL https://releases.hashicorp.com/vagrant/2.2.19/vagrant_2.2.19_x86_64.deb -o /tmp/vagrant.deb
+    dpkg -i /tmp/vagrant.deb
+    rm /tmp/vagrant.deb
 }
 
 INSTALL_DOCKER_ENGINE() {
     printf "${YELLOW}Installing Docker...${NC}\n"
-    # curl -fsSL https://get.docker.com -o get-docker.sh
-    # sh get-docker.sh
-    # printf "${YELLOW}Adding $USER to docker group...\n"
-    # usermod -aG docker $USER
-    # printf "${YELLOW} Enabling docker service...\n"
-    # systemctl enable docker
-    # printf "${YELLOW} Starting docker service...${NC}\n"
-    # systemctl start docker
-    # rm get-docker.sh
+    curl -fsSL https://get.docker.com -o get-docker.sh
+    sh get-docker.sh
+    printf "${YELLOW}Adding $USER to docker group...\n"
+    usermod -aG docker $USER
+    printf "${YELLOW} Enabling docker service...\n"
+    systemctl enable docker
+    printf "${YELLOW} Starting docker service...${NC}\n"
+    systemctl start docker
+    rm get-docker.sh
 }
 
 INSTALL_VIRTUALBOX() {
     printf "${YELLOW}Installing VirtualBox...${NC}\n"
-    # curl -fsSL https://download.virtualbox.org/virtualbox/6.1.4/virtualbox-6.1_6.1.4-139181~Ubuntu~bionic_amd64.deb -o /tmp/virtualbox.deb
-    # dpkg -i /tmp/virtualbox.deb
-    # rm /tmp/virtualbox.deb
+    curl -fsSL https://download.virtualbox.org/virtualbox/6.1.4/virtualbox-6.1_6.1.4-139181~Ubuntu~bionic_amd64.deb -o /tmp/virtualbox.deb
+    dpkg -i /tmp/virtualbox.deb
+    rm /tmp/virtualbox.deb
 }
 
 if [[ $EUID -ne 0 ]]; then  
@@ -108,7 +108,7 @@ else
     printf "${GREEN}*  INSTALLING ESSENTIAL PACKAGES & OPTIONAL PROGRAMS   *${NC}\n"
     printf "${GREEN}********************************************************${NC}\n"
     printf "${BLUE}Updating apt-get...${NC}\n"
-   #apt update 
+    apt update 
     printf "${GREEN}--------------------------------------------------------${NC}\n"
     printf "${BLUE}This script will install the following packages: ${NC}\n\n"
     printf "[ ${BLUE}${ESSENTIALS[*]}${NC} ]\n\n"
